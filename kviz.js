142dc9b61727babb868997e05300a6181547d39e
zsoltv88 <148365216+zsoltv88@users.noreply.github.com>
2023-11-02 15:43:52 +0100

    js kérdések megadása

---
+++ kviz.js
@@ -0,0 +1,12 @@
+const orszagok = {
+    "Magyarország": "Budapest",
+    "Németország": "Berlin",
+    "Ausztria": "Bécs",
+    "Csehország": "Prága",
+    "Spanyolország": "Madrid",
+    "Olaszország": "Róma",
+    "Svédország": "Stockholm",
+    "Finnország": "Helsinki",
+    "Franciaország": "Párizs",
+    "Egyesült Királyság": "London"
+};
