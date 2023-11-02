1c6f60321453175451bb8d0a0a426405e3a147d3
zsoltv88 <148365216+zsoltv88@users.noreply.github.com>
2023-11-02 16:37:11 +0100

    állandók definiálása

--- kviz.js
+++ kviz.js
@@ -10,3 +10,18 @@ const orszagok = {
     "Franciaország": "Párizs",
     "Egyesült Királyság": "London"
 };
+
+const kerdes = document.getElementById("kerdes");
+const answer = document.getElementById("answer");
+const ellenoriz = document.getElementById("ellenoriz");
+const valasz = document.getElementById("valasz");
+const pont = document.getElementById("eredmeny");
+
+let orszag = 0;
+let valasz = 0;
+let pont = 0;
+let kerdesek = 0;
+
+function kerdesKiir() {
+
+}
